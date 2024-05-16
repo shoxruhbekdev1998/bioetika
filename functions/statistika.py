d13877d4d9e8f7653bc739cac2661021774ec73e
shoxruhbekdev1998 <145317548+shoxruhbekdev1998@users.noreply.github.com>
2024-05-16 13:28:24 +0500

    Update statistika.py

--- functions/statistika.py
+++ functions/statistika.py
@@ -35,6 +35,22 @@ def all_satatistikas(topic_id,question_id,topic_name,question_name, id, from_dat
 
 def statistika_adding(topic_id,question_id,topic_name, question_name, answer_a, answer_b, answer_c, answer_d, answer_e, answer_f, db):
     
+statistikas = db.query(Statistika).filter(Statistika.topic_name == topic_name,Statistika.question_name == question_name).first()
+
+    if statistikas is None:
+        statistikas = Statistika(
+            topic_id=topic_id,
+            question_id=question_id,
+            topic_name=topic_name,
+            question_name=question_name,
+            answer_a=0,
+            answer_b=0,
+            answer_c=0,
+            answer_d=0,
+            answer_e=0,
+            answer_f=0
+        )
+        db.add(statistikas)
 
 
 
@@ -51,26 +67,13 @@ def statistika_adding(topic_id,question_id,topic_name, question_name, answer_a, 
 
 
 def add_statistikas(form, db):
-    new_statistika=Statistika(
-        topic_id=form.topic_id,
-        question_id=form.question_id,
-        topic_name=form.topic_name,
-        question_name=form.question_name,
-        answer_a=form.answer_a,
-        answer_b=form.answer_b,
-        answer_c=form.answer_c,
-        answer_d=form.answer_d,
-        answer_e=form.answer_e,
-        answer_f=form.answer_f,
-        db=db
-    )
-
-    db.add(new_statistika)
-    db.commit()
-    db.refresh(new_statistika)
     statistika_adding(topic_id=form.topic_id, question_id=form.question_id,topic_name=form.topic_name, question_name=form.question_name, answer_a=form.answer_a, answer_b=form.answer_b, answer_c=form.answer_c, answer_d=form.answer_d, answer_e=form.answer_e, answer_f=form.answer_f, db=db)
 
 
+
+
+    db.commit()
+   
 
     return {"data": "Statistika add base"}
 
